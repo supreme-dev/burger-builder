094e8873f5b2e23342077469ef57bbe315ce956c
shobanaa <shobanaamail@gmail.com>
2020-07-22 14:28:15 +0530

    Add toolbar

---
+++ src/components/Navigation/Toolbar.jsx
@@ -0,0 +1,12 @@
+// vendor import
+import React from 'react';
+
+const toolbar = (props) => (
+    <header>
+        <div>Menu</div>
+        <div>Logo</div>
+    </header>
+);
+
+// export
+export default toolbar;
